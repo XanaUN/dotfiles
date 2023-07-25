5c0c4f7c4170256ebf4cecc9ebe97aeaadbb4cf7
XanaUN <Xanaun@bk.ru>
2023-07-25 23:28:36 +0700

    Добавить oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-
 git clone git@github.com:XanaUN/dotfiles.git  "${HOME}/dotfiles"
 
-# configs
 ln -sf "${HOME}/dotfiles/configs/.zshrc" "${HOME}/.zshrc"
+ln -sf "${HOME}/dotfiles/configs/.oh-my-zsh" "${HOME}/.oh-my-zsh"
 ln -sf "${HOME}/dotfiles/configs/.gitconfig" "${HOME}/.gitconfig"
